adfd684eabecabd544bb95b2d89ef32186c8e507
MikeForde <mikeforde@ymail.com>
2024-08-08 11:17:08 +0000

    meta and contact text

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -9,7 +9,7 @@
     <section class="info-section" ref="section1" :data-section="1" :class="{ 'fade-in': showSection1 }">
       <div class="info-card">
         <h2>Booking Sessions</h2>
-        <p>Please contact me for my availability and to book therapy sessions*. Text and email are my preferred methods of contact as I may be in a session if you call. I will then arrange a 15-minute, free introductory phone call, so that we can get a sense of each other and see if we are a good fit.</p>
+        <p>Please contact me for my availability and to book therapy sessions*. Text and email are my preferred methods of contact as I may be in a session if you call. I will then arrange a 15-minute, free introductory phone call, to ask any questions you may have, discuss the outcome you’re looking for, or explore how therapy will work for you.</p>
         <p><i>* I offer free counselling but ask my clients to contribute £12 per 1-hour session towards room hire.</i></p>
         <p>Email: <a href="mailto:clearskiespractice@gmail.com">clearskiespractice@gmail.com</a></p>
         <p>Phone/text: 07760 531057</p>

35c40e527c9309c81eca44fc9d8610efa93d726c
MikeForde <mikeforde@ymail.com>
2025-06-10 15:18:56 +0000

    Reduced images

--- src/components/TherapyPage.vue
+++ src/components/TherapyPage.vue
@@ -17,7 +17,7 @@
     <section class="therapy" ref="therapy" :class="{ 'fade-in': showTherapy }">
       <div class="therapy-container">
         <div class="therapy-card image-card">
-          <img src="@/assets/Therapy_Stones.png" alt="Therapy Image" class="therapy-image" />
+          <img src="@/assets/Therapy_Stones.jpg" alt="Therapy Image" class="therapy-image" />
         </div>
         <div class="therapy-card text-card">
           <h2>Therapy</h2>
@@ -91,7 +91,7 @@
             wellbeing beyond therapy sessions.</p>
         </div>
         <div class="approach-card image-card">
-          <img src="@/assets/TherapyPath.png" alt="Therapy Path" class="therapy-path-image" />
+          <img src="@/assets/TherapyPath.jpg" alt="Therapy Path" class="therapy-path-image" />
         </div>
       </div>
     </section>

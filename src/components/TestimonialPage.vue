ebd7fef3d1d9044ba898f68dab13a4479f9f36f8
MikeForde <mikeforde@ymail.com>
2025-07-15 11:44:10 +0000

    New testimonial

--- src/components/TestimonialPage.vue
+++ src/components/TestimonialPage.vue
@@ -23,7 +23,7 @@
     >
       <div class="testimonial-card">
         <blockquote>
-          “Galina was very professional but also very understanding and helpful to me! She asked very relevant questions and listened intently!”
+          “I cannot put into words how much Galina has supported me throughout our sessions. This was my first time attending therapy so I was apprehensive. She guided me through everything and after the first session I felt at ease and excited to come back the following week. From the rewinds to coping strategies, she has given me the tools and resources to help me moving forward. Couldn’t recommend her highly enough.”
         </blockquote>
         <img src="@/assets/CSP_Logo_Black_trans.png" alt="CSP Logo" class="testimonial-logo" />
       </div>
@@ -37,7 +37,7 @@
     >
       <div class="testimonial-card">
         <blockquote>
-          “Galina is kind, patient, caring and enables you to confront your issues. I cannot ever thank her enough for setting me free of my issues and with her help and guidance, she was like a beacon. I had a rewind which was instantly successful, it was truly mind-blowing the difference it made and continues to make. I am now back to the bubbly, positive person I was with Galina's gentle guidance.”
+          “Galina was very professional but also very understanding and helpful to me! She asked very relevant questions and listened intently!”
         </blockquote>
         <img src="@/assets/CSP_Logo_White_trans.png" alt="CSP Logo" class="testimonial-logo" />
       </div>
@@ -51,13 +51,27 @@
     >
       <div class="testimonial-card">
         <blockquote>
+          “Galina is kind, patient, caring and enables you to confront your issues. I cannot ever thank her enough for setting me free of my issues and with her help and guidance, she was like a beacon. I had a rewind which was instantly successful, it was truly mind-blowing the difference it made and continues to make. I am now back to the bubbly, positive person I was with Galina's gentle guidance.”
+        </blockquote>
+        <img src="@/assets/CSP_Logo_Black_trans.png" alt="CSP Logo" class="testimonial-logo" />
+      </div>
+    </section>
+
+    <section
+      class="testimonial-section"
+      ref="section4"
+      :data-section="4"
+      :class="{ 'fade-in': showSection4 }"
+    >
+      <div class="testimonial-card">
+        <blockquote>
           <p>“After my wife of 25 years died suddenly I thought I should be old enough to deal with the grief on my own. However, the long years of interdependency were too ingrained to just suddenly let go. I found myself becoming more and more introverted, I stopped interacting with friends, became inactive and depressed. It wasn’t until I started having thoughts of self-harm that I finally took notice and realised I needed help. I just didn’t want to exist without my wife any longer.</p>
           <p>Just before Christmas 2024 I saw a leaflet simply stating “Galina - Bereavement Therapy”. At that moment, I reached rock bottom and called Galina as a last hope… and what a good call that turned out to be!</p>
           <p>Galina is quiet, calm, empathic and very, very clever. She seemed to understand my total desolation from my first visit. At first, she simply listened. I didn’t realise it at the time but she simply gave me the space to spill all the horror, grief and solitude I had felt since my wife’s death. Then, slowly and without pressure, we began to talk and she started to offer me tools to help me through. Never any judgement; never any pressure; Galina proved to be the mirror in which I faced myself.</p>
           <p>Happily, I have now been able to look beyond my grief; to accept and understand the value of past experience and to carry that into the future with me. I will never forget the incredibly good times that my marriage brought me and those memories will continue to nourish me and overshadow my grief.</p>
           <p>To Galina, my most heartfelt thanks. To you reading this, do not suffer alone. There is a path through grief but I only found mine with Galina’s help.”</p>
         </blockquote>
-        <img src="@/assets/CSP_Logo_Black_trans.png" alt="CSP Logo" class="testimonial-logo" />
+        <img src="@/assets/CSP_Logo_White_trans.png" alt="CSP Logo" class="testimonial-logo" />
       </div>
     </section>
   </div>
@@ -72,6 +86,7 @@ export default {
       showSection1: false,
       showSection2: false,
       showSection3: false,
+      showSection4: false,
       videoPlayable: true,
       backupImage: require('@/assets/AnimatedSky.gif')
     };
@@ -93,6 +108,7 @@ export default {
       observer.observe(this.$refs.section1);
       observer.observe(this.$refs.section2);
       observer.observe(this.$refs.section3);
+      observer.observe(this.$refs.section4);
     });
   },
   methods: {
@@ -173,7 +189,7 @@ export default {
   color: #666;
 }
 
-.testimonial-section[data-section="2"] {
+.testimonial-section[data-section="2"], .testimonial-section[data-section="4"]{
   background-color: #5DAED5;
   color: white;
 }
